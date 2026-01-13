054f81fd35d43ef0969b2a57e9e2e759a2faac73
pankudlaty <pkownacki90@gmail.com>
2026-01-13 15:40:56 +0100

    Working on LeafNode

---
+++ src/leafnode.py
@@ -0,0 +1,10 @@
+from htmlnode import HTMLNode
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError
